7f67d50c6766605f32c632a1b5d8cf2c53e256f6
Preston Davies <pdavies@Prestons-Air-M2.fios-router.home>
2024-01-16 22:53:11 -0500

    Set up Clerk and Header/Footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,5 +1,23 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 const Footer = () => {
-  return <footer className='w-full border-b'>footer</footer>;
+  return (
+    <footer className='border-t'>
+      <div className='flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row'>
+        <Link href='/'>
+          <Image
+            src='/assets/images/logo.svg'
+            alt='logo'
+            width={128}
+            height={38}
+          />
+        </Link>
+
+        <p>2024 Event Machine. All Rights reserved.</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
